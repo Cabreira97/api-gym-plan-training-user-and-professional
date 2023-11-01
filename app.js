375ac1ff7671b04a7cda9b022e6c5cff0b36bd70
Cabreira97 <thiagovieiramachado97@gmail.com>
2023-10-31 23:12:46 -0300

    ✨ Add app.

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import cors from "cors"
+import routes from './routes.js'
+
+import { sequelize } from './databases/conecta.js'
+import { Aluno } from './models/Aluno.js'
+import { Treino } from './models/Treino.js'
+import { Profissional } from './models/Profissional.js'
+import { Plano } from './models/Plano.js'
+
+
+const app = express()
+const port = 4000
+
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+
+async function conecta_db() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com banco de dados realizada com sucesso');
+    await Plano.sync()
+    await Aluno.sync()
+    await Profissional.sync()
+    await Treino.sync()
+
+  
+    console.log("Ok! Tabelas Plano, Aluno, Profissional e Treino sincronizadas com sucesso")
+//    await Reserva.sync();  // cria a tabela no banco (a partir dos modelos - se não existirem)
+//    await Reserva.sync({alter: true});  // Verifica se há alterações e atualiza as tabelas se houver
+//    await Reserva.sync({force: true});  // recria as tabelas, mesmo se já existirem
+  } catch (error) {
+    console.error('Erro na conexão com o banco: ', error);
+  }
+}
+conecta_db()
+
+app.get('/', (req, res) => {
+  res.send('API Academia')
+})
+
+app.listen(port, () => {
+  console.log(`Servidor Rodando na Porta: ${port}`)
+})
